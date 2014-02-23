02dbd0290717bb963590c6b1008c14fba8a633e6
Ian <ian@ian-VirtualBox.(none)>
2014-02-23 13:29:18 -0600

    reworked file to seperate modules and added test for player movement

--- game/src/player.py
+++ game/src/player.py
@@ -3,16 +3,9 @@ from pygame.locals import *
 import spritesheet
 from SpriteSheetAnim import SpriteStripAnim
 
-
 height = 10
 width = 10
 BLOCK_SIZE = 60
-screen = pygame.display.set_mode((width*BLOCK_SIZE, height*BLOCK_SIZE))
-clock = pygame.time.Clock()
-BLACK = (0,0,0)
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill(BLACK)
 
 class PlayerSprite (pygame.sprite.Sprite):
     def __init__ (self, image, position):
@@ -43,7 +36,6 @@ class PlayerSprite (pygame.sprite.Sprite):
             self.direction = "up"
             if self.currentStrip is self.strips[self.direction]:
                 self.image = self.currentStrip.next()
-                print "Right"
             else:
                 self.currentStrip = self.strips[self.direction]
                 self.image = self.currentStrip.next()
@@ -52,7 +44,6 @@ class PlayerSprite (pygame.sprite.Sprite):
             y += 1
             self.direction  = "down"
             if self.currentStrip is self.strips[self.direction]:
-                print "Down"
                 self.image = self.currentStrip.next()
             else:
                 self.currentStrip = self.strips[self.direction]
@@ -62,7 +53,6 @@ class PlayerSprite (pygame.sprite.Sprite):
             x -= 1
             self.direction = "left"
             if self.currentStrip is self.strips[self.direction]:
-                print "Left"
                 self.image = self.currentStrip.next()
             else:
                 self.currentStrip = self.strips[self.direction]
@@ -72,7 +62,6 @@ class PlayerSprite (pygame.sprite.Sprite):
             x += 1
             self.direction = "right"
             if self.currentStrip is self.strips[self.direction]:
-                print "Right"
                 self.image = self.currentStrip.next()
             else:
                 self.currentStrip = self.strips[self.direction]
@@ -89,92 +78,3 @@ class PlayerSprite (pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.currentStrip = self.strips[self.direction]
         self.rect.center = self.position
-
-class ObjectSprite (pygame.sprite.Sprite):
-    normal = pygame.image.load('crate.png')
-    hit = pygame.image.load('burning_crate.png')
-
-    def __init__ (self, position):
-        pygame.sprite.Sprite.__init__(self)
-        self.coords = position
-        self.position = (((self.coords[0] * BLOCK_SIZE) + (BLOCK_SIZE/2)), ((self.coords[1] * BLOCK_SIZE) + (BLOCK_SIZE/2)))
-        self.image = self.normal
-        self.rect = self.image.get_rect()
-        self.rect.center = self.position
-
-    def update (self, hit_list):
-        if self in hit_list:
-            self.image = self.hit
-
-        self.rect = self.image.get_rect()
-        self.rect.center = self.position
-
-crates = [
-    ObjectSprite((1, 1)),
-    ObjectSprite((5, 7)),
-    ObjectSprite((2, 3)),
-    ObjectSprite((9, 5)),
-]
-
-crates_2 = [
-    ObjectSprite((9, 2)),
-    ObjectSprite((3, 6)),
-    ObjectSprite((7, 4)),
-    ObjectSprite((8, 8)),
-]
-crate_group = pygame.sprite.RenderPlain(*crates)
-crate_group_2 = pygame.sprite.RenderPlain(*crates_2)
-player = PlayerSprite('Hero.png', (5, 5))
-player_group = pygame.sprite.RenderPlain(player)
-
-def did_collide(sprite_one, sprite_two):
-    if sprite_one.coords == sprite_two.coords: return True
-    else: return False
-
-screen.blit(background, (0, 0))
-pygame.display.flip()
-while 1:
-    deltat = clock.tick(10)
-    for event in pygame.event.get():
-        if not hasattr(event, 'key'): continue
-        if event.key == K_ESCAPE: sys.exit(0)
-        elif event.type == KEYDOWN:
-            if event.key == K_d:
-                player.right = True
-                player.left = False
-                #player.up = False
-                #player.down = False
-            elif event.key == K_a:
-                player.left = True
-                player.right = False
-                #player.up = False
-                #player.down = False
-            elif event.key == K_w:
-                player.up = True
-                player.down = False
-                #player.left = False
-                #player.right = False
-            elif event.key == K_s:
-                player.down = True
-                player.up = False
-                #player.left = False
-                #player.right = False
-        elif event.type == KEYUP:
-            if event.key == K_d:
-                player.right = False
-            elif event.key == K_a:
-                player.left = False
-            elif event.key == K_w:
-                player.up = False
-            elif event.key == K_s:
-                player.down = False
-
-    player_group.update(deltat)
-    collisions = pygame.sprite.spritecollide(player, crate_group, False, did_collide)
-    collide = pygame.sprite.spritecollide(player, crate_group_2, True, did_collide)
-    crate_group.update(collisions)
-    screen.blit(background, (0, 0))
-    crate_group.draw(screen)
-    crate_group_2.draw(screen)
-    player_group.draw(screen)
-    pygame.display.flip()
