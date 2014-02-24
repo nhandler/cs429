3bc4d4865d92ef5343077402db0337daa986dde8
Matt Verzak <verzak1@illinois.edu>
2014-02-24 16:51:00 -0600

    verzak1_branch refactored movement

--- game/src/movement.py
+++ game/src/movement.py
@@ -1,6 +1,8 @@
-import pygame, math, sys
+import pygame 
+import math
+import sys
 from pygame.locals import *
-from player import PlayerSprite
+from player import PlayerSprite, Direction, HorizontalMovement, VerticalMovement
 from crate import ObjectSprite
 
 height = 10
@@ -34,48 +36,58 @@ def did_collide(sprite_one, sprite_two):
     if sprite_one.coords == sprite_two.coords: return True
     else: return False
 
+def did_crate_collide(sprite_one, crate_sprite):
+    if sprite_one.coords == crate_sprite.coords:
+        crate_sprite.takeHit()
+        return True
+    else: 
+        return False
+
 screen.blit(background, (0, 0))
 pygame.display.flip()
+
+keydown_moves = { K_d : (player.changeHorizontalMovement, HorizontalMovement.right), K_a : (player.changeHorizontalMovement, HorizontalMovement.left), 
+                  K_w : (player.changeVerticalMovement, VerticalMovement.up), K_s : (player.changeVerticalMovement, VerticalMovement.down)}
+
+keyup_moves = { K_d : (player.changeHorizontalMovement, HorizontalMovement.none), K_a : (player.changeHorizontalMovement, HorizontalMovement.none), 
+                K_w : (player.changeVerticalMovement, VerticalMovement.none), K_s : (player.changeVerticalMovement, VerticalMovement.none)}
+
 while 1:
     deltat = clock.tick(10)
     for event in pygame.event.get():
         if not hasattr(event, 'key'): continue
         if event.key == K_ESCAPE: sys.exit(0)
         elif event.type == KEYDOWN:
+            (move, arg) = keydown_moves.get(event.key, (lambda arg: None, 0))
+            move(arg)
+            '''
             if event.key == K_d:
-                player.right = True
-                player.left = False
-                #player.up = False
-                #player.down = False
+                player.horizontalMovement = HorizontalMovement.right
             elif event.key == K_a:
-                player.left = True
-                player.right = False
-                #player.up = False
-                #player.down = False
+                player.horizontalMovement = HorizontalMovement.left
             elif event.key == K_w:
-                player.up = True
-                player.down = False
-                #player.left = False
-                #player.right = False
+                player.verticalMovement = VerticalMovement.up
             elif event.key == K_s:
-                player.down = True
-                player.up = False
-                #player.left = False
-                #player.right = False
+                player.verticalMovement = VerticalMovement.down
+            '''
         elif event.type == KEYUP:
+            (move, arg) = keyup_moves.get(event.key, (lambda arg: None, 0))
+            move(arg)
+            '''
             if event.key == K_d:
-                player.right = False
+                player.horizontalMovement = HorizontalMovement.none
             elif event.key == K_a:
-                player.left = False
+                player.horizontalMovement = HorizontalMovement.none
             elif event.key == K_w:
-                player.up = False
+                player.verticalMovement = VerticalMovement.none
             elif event.key == K_s:
-                player.down = False
+                player.verticalMovement = VerticalMovement.none
+            '''
 
     player_group.update(deltat)
-    collisions = pygame.sprite.spritecollide(player, crate_group, False, did_collide)
+    collisions = pygame.sprite.spritecollide(player, crate_group, False, did_crate_collide)
     collide = pygame.sprite.spritecollide(player, crate_group_2, True, did_collide)
-    crate_group.update(collisions)
+    #crate_group.update(collisions)
     screen.blit(background, (0, 0))
     crate_group.draw(screen)
     crate_group_2.draw(screen)
