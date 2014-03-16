2e074bbfde06f54d3a411b412f3479a330276092
Jonathan <jonathan.albrecht1@gmail.com>
2014-03-16 12:48:42 -0500

    Moved movement tests to creature test file instead of player

--- game/test/test_creature.py
+++ game/test/test_creature.py
@@ -3,34 +3,103 @@ import math
 import sys
 sys.path.append('../src')
 import unittest
+from locals import Direction
+from creature import CreatureSprite
 
-class TestCreatur(unittest.TestCase):
+class TestCreature(unittest.TestCase):
 
+    creature = None
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()
+    BLACK = (0,0,0)
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill(BLACK)
 
     def setUp(self):
-        pass
+        self.creature = CreatureSprite('Hero.png', (5, 5), (60, 60))
 
     def test_move_up(self):
-        pass
+        self.creature.move(Direction.up);
+        self.assertEqual((5, 4), self.creature.coords)
 
     def test_move_down(self):
-        pass
+        self.creature.move(Direction.down);
+        self.assertEqual((5, 6), self.creature.coords)
 
     def test_move_left(self):
-        pass
+        self.creature.move(Direction.left);
+        self.assertEqual((4, 5), self.creature.coords)
 
     def test_move_right(self):
-        pass
+        self.creature.move(Direction.right);
+        self.assertEqual((6, 5), self.creature.coords)
 
     def test_top_edge(self):
-        pass
+        self.creature.move(Direction.up)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.up)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.up)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.up)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.up)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.up);
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.up)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.assertEqual((5, 0), self.creature.coords)
 
     def test_bottom_edge(self):
-        pass
+        self.creature.move(Direction.down)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.down)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.down)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.down)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.down)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.down)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.down)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.assertEqual((5, 9), self.creature.coords)
 
     def test_right_edge(self):
-        pass
+        self.creature.move(Direction.right)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.right)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.right)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.right)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.right)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.right)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.right)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.assertEqual((9, 5), self.creature.coords)
 
     def test_left_edge(self):
-        pass
+        self.creature.move(Direction.left)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.left)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.left)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.left)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.left)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.left)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.creature.move(Direction.left)
+        self.creature.isOutOfBounds(10, 10, -1, -2, -1, -2)
+        self.assertEqual((0, 5), self.creature.coords)
 
--- game/test/test_player.py
+++ game/test/test_player.py
@@ -3,10 +3,12 @@ from pygame.locals import *
 sys.path.append('../src')
 import unittest
 from player import PlayerSprite, Direction
+from item import Item, MagicShoes
 
-class TestPlayerMovement(unittest.TestCase):
+class TestPlayer(unittest.TestCase):
 
     player = None
+    item = None
     screen = pygame.display.set_mode((600, 600))
     clock = pygame.time.Clock()
     BLACK = (0,0,0)
@@ -16,98 +18,12 @@ class TestPlayerMovement(unittest.TestCase):
 
     def setUp(self):
         self.player = PlayerSprite('Hero.png', (5, 5), (60, 60))
+        self.item = MagicShoes()
 
-    def test_move_up(self):
-        """self.player.move(Direction.up);
-        self.assertEqual((5, 4), self.player.coords)"""
-	self.assertEqual(1, 1)
-
-    def test_move_down(self):
-        """self.player.move(Direction.down);
-        self.assertEqual((5, 6), self.player.coords)"""
-	self.assertEqual(1, 1)
-
-    def test_move_left(self):
-        """self.player.move(Direction.left);
-        self.assertEqual((4, 5), self.player.coords)"""
-	self.assertEqual(1, 1)
-
-    def test_move_right(self):
-        """self.player.move(Direction.right);
-        self.assertEqual((6, 5), self.player.coords)"""
-	self.assertEqual(1, 1)
-
-    def test_top_edge_collision(self):
-        """self.player.move(Direction.up);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.up);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.up);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.up);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.up);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.up);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.up);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.assertEqual((5, 0), self.player.coords)"""
-	self.assertEqual(1, 1)
-
-    def test_bottom_edge_collision(self):
-        """self.player.move(Direction.down);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.down);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.down);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.down);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.down);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.down);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.down);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.assertEqual((5, 9), self.player.coords)"""
-	self.assertEqual(1, 1)
-
-    def test_right_edge_collision(self):
-        """self.player.move(Direction.right);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.right);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.right);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.right);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.right);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.right);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.right);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.assertEqual((9, 5), self.player.coords)"""
-	self.assertEqual(1, 1)
-
-    def test_left_edge_collision(self):
-        """self.player.move(Direction.left);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.left);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.left);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.left);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.left);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.left);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.player.move(Direction.left);
-        self.player.isOutOfBounds(10, 10, -1, -2, -1, -2);
-        self.assertEqual((0, 5), self.player.coords)"""
-	self.assertEqual(1, 1)
+    def test_AddItemToInventory(self):
+        self.player.addItemToInventory(self.item)
+        inventory = self.player.inventory
+        self.assertEqual(inventory["Magic Shoes"], self.item.type)
 
 if __name__ == '__main__':
     unittest.main()
