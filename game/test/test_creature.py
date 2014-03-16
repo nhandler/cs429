819edf283b725e233e62a72a826ba213430b3df3
Jonathan <jonathan.albrecht1@gmail.com>
2014-03-15 19:09:50 -0500

    Stubbed version of creature test

---
+++ game/test/test_creature.py
@@ -0,0 +1,36 @@
+import pygame
+import math
+import sys
+sys.path.append('../src')
+import unittest
+
+class TestCreatur(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+    def test_move_up(self):
+        pass
+
+    def test_move_down(self):
+        pass
+
+    def test_move_left(self):
+        pass
+
+    def test_move_right(self):
+        pass
+
+    def test_top_edge(self):
+        pass
+
+    def test_bottom_edge(self):
+        pass
+
+    def test_right_edge(self):
+        pass
+
+    def test_left_edge(self):
+        pass
+
